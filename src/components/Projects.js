e19667200d001ee4e67ca0830411ba265417a88c
aakashsharma7 <aakashsharma.ghd@gmail.com>
2024-05-16 23:26:33 +0530

    js

---
+++ src/components/Projects.js
@@ -0,0 +1,103 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Airbnb",
+      description: "Nextjs, Reactjs, TailwindCSS, Prisma, MongoDB, NextAuth",
+      imgUrl: projImg1,
+      Link: "https://github.com/aakashsharma7?tab=repositories",
+    },
+    {
+      title: "Gemini AI Clone",
+      description: "Reactjs, Vite, Gemini API",
+      imgUrl: projImg2,
+      Link: "https://github.com/aakashsharma7/Gemini_Clone",
+    },
+    {
+      title: "Apple Vision Pro",
+      description: "JavaScript, Gsap",
+      imgUrl: projImg3,
+      Link: "https://github.com/aakashsharma7/BrainWave",
+    },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg1,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg2,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg3,
+    // },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>A MERN stack developer specializes in web development using MongoDB, Express.js, React.js, and Node.js. They build full-stack applications, handling both the frontend and backend development. 
+                  Their expertise includes designing database schemas, creating RESTful APIs, and crafting interactive user interfaces with React.js. 
+                  MERN stack developers are adept at integrating different technologies to create seamless and efficient web applications.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    {/* <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item> */}
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p></p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p></p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,67 @@
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <p>The MERN stack, consisting of MongoDB, Express.js, React.js, and Node.js, equips developers with a comprehensive toolkit for building dynamic and scalable web applications. 
+                          Skills in the MERN stack include proficiency in MongoDB for efficient NoSQL database management and data modeling, and expertise in Express.js for creating robust server-side logic and APIs. 
+                          React.js skills involve developing interactive and responsive user interfaces using a component-based architecture and effective state management. Node.js proficiency encompasses writing server-side JavaScript, handling asynchronous operations, and managing dependencies. Together, 
+                          these skills enable developers to seamlessly integrate front-end and back-end technologies, delivering high-performance web applications.</p>
+                        <Carousel responsive={responsive} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>JavaScript</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Data Structure</h5>
+                            </div>
+                            {/* <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div> */}
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img className="background-image-left" src={colorSharp} alt="Image" />
+    </section>
+  )
+}
